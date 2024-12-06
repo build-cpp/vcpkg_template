c4b539b4251b4d7102b5d239e1439795df663c3f
Duncan Ogilvie <mr.exodia.tpodt@gmail.com>
2024-12-06 17:41:52 +0100

    Update cmkr and showcase latest features

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
-#include <cstdio>
 #include <fmt/format.h>
 #include <sqlite3.h>
+#include <mylib/version.hpp>
+#include <mylib/stringutils.hpp>
 
 int main()
 {
     fmt::print("SQLite version: {}\n", sqlite3_libversion());
+    fmt::print("{} version: {}\n", mylib::reverse("bilym"), mylib::version());
 }
